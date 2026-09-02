6a574a0a05963ae5294dab24109f0725a5b353c6
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2026-09-02 17:05:31 +0530

    fixed page load time

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ const path = isPage ? "../" : "./";
 async function loadComponent(elementId, filePath) {
   const element = document.getElementById(elementId);
   if (!element) return;
+
   try {
-    const response = await fetch(filePath);
+    const response = await fetch(filePath, { cache: "force-cache" });
 
     if (!response.ok) {
       throw new Error(`Failed to load ${filePath}`);
     }
+
     element.innerHTML = await response.text();
   } catch (error) {
     console.log(error);
@@ -21,6 +23,7 @@ async function showToast(message, title = "Notification") {
   if (!toastElement) {
     await loadComponent("toast", `${path}components/toast.html`);
   }
+
   const toastNode = document.getElementById("appToast");
   const toastMessage = document.getElementById("toast-message");
   const toastTitle = document.getElementById("toast-title");
@@ -28,8 +31,10 @@ async function showToast(message, title = "Notification") {
   if (!toastNode || !toastMessage || !toastTitle) {
     return;
   }
+
   toastMessage.textContent = message;
   toastTitle.textContent = title;
+
   const toast = bootstrap.Toast.getOrCreateInstance(toastNode);
   toast.show();
 }
@@ -37,14 +42,16 @@ async function showToast(message, title = "Notification") {
 function showStoredToast() {
   const message = localStorage.getItem("toastMessage");
   const title = localStorage.getItem("toastTitle") || "Notification";
+
   if (!message) return;
+
   showToast(message, title);
   localStorage.removeItem("toastMessage");
   localStorage.removeItem("toastTitle");
 }
 
-loadComponent("navbar", `${path}components/navbar.html`);
-loadComponent("footer", `${path}components/footer.html`);
-loadComponent("toast", `${path}components/toast.html`).then(() =>
-  showStoredToast(),
-);
+Promise.all([
+  loadComponent("navbar", `${path}components/navbar.html`),
+  loadComponent("footer", `${path}components/footer.html`),
+  loadComponent("toast", `${path}components/toast.html`) 
+]).then(() => showStoredToast());
